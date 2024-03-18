09e4d834df463345821dea9eb80c5832f837453b
ad3rinto <aad3rinto@gmail.com>
2024-03-18 19:38:43 +0000

    format response to show if it rains at particular hours

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # f = open("file.txt", "r")
 # APPID = f.readline()
 # f.close()
 
-URL = "https://api.openweathermap.org/data/2.5/weather"
+URL = "https://api.openweathermap.org/data/2.5/forecast"
 LAT = 53.380149
 LON = -2.193189
 
@@ -20,4 +21,12 @@ SITE_PARAMS = {
 r = requests.get(URL, params=SITE_PARAMS)
 r.status_code
 data = r.json()
-print(data)
+list_of_hourly = data["list"][0:5]
+
+is_rain = "rain"
+
+for i in list_of_hourly:
+    if i.get(is_rain):
+        print(f"E go rain o, for {i["dt_txt"].split()[1]}")
+    else:
+        print(f"No rain at {i["dt_txt"].split()[1]}")
