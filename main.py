a635d1ad907dbd153c8d72d4006f472dfe0b6d02
ad3rinto <aad3rinto@gmail.com>
2024-03-20 12:32:45 +0000

    refactored code to only print one statement

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 URL = "https://api.openweathermap.org/data/2.5/forecast"
 LAT = 53.380149
 LON = -2.193189
@@ -12,7 +11,7 @@ LON = -2.193189
 SITE_PARAMS = {
     "lat": LAT,
     "lon": LON,
-    "cnt":4,
+    "cnt": 4,
     "appid": os.getenv("APPID")
 }
 
@@ -20,9 +19,11 @@ r = requests.get(URL, params=SITE_PARAMS)
 r.status_code
 data = r.json()
 list_of_hourly = data["list"]
+will_rain = False
 
 for p in list_of_hourly:
-    if p["weather"][0]["id"] < 700:
-        print(f"It is forecasted to rain at {p["dt_txt"].split()[1]} hrs")
-    else:
-        print(f'It is not forecasted to rain at {p["dt_txt"].split()[1]} hrs')
+    if int(p["weather"][0]["id"]) < 700:
+        will_rain = True
+
+if will_rain:
+    print(f"It is forecasted to rain at {p["dt_txt"].split()[1]} hrs")
